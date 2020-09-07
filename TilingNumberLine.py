637cda1b1fe2c8ec2aaa54a5921a5f68cd16a35c
Artem Bolshakov <a.t.bolsh@gmail.com>
2020-09-07 16:44:36 -0400

    Environment and tiling object written

---
+++ TilingNumberLine.py
@@ -0,0 +1,65 @@
+import numpy as np
+from copy import deepcopy
+
+
+# Note that allowed states include 0 but not self.size
+
+class NumLineTiling:
+
+    def __init__(self, size=1000, blockSize=100, interval=10, alpha = 0.1):
+        self.size = size
+        self.blockSize = blockSize
+        self.interval = interval
+        self.alpha = alpha
+        
+        self.numBlocks = 1 + max(int((size - blockSize)/interval), 0)
+        self.blockWeights = np.zeros(self.numBlocks, dtype='float64')
+        ## Possible randomization commes here
+        self.initBlockWeights()
+
+
+    def initBlockWeights(self):
+        return None # No change for now; might add randomization later.
+    
+    
+    def getBlocks(self, state):
+        if (state >= self.size) or (state < 0):
+            return -1, -1
+        
+        highest = int(state / self.interval) #Highest index of a block that contains state
+
+        # At interval = 10 and blockSize = 100, I don't want state 100 to be in block 0;
+        # each block gets exactly 100 states
+        lowest  = int((self.interval + state - self.blockSize) / self.interval) 
+        
+        lowest = max(lowest, 0)
+        highest = min(highest, self.numBlocks - 1)
+        
+        return lowest, highest + 1 #Plus one is there for slicing purposes.
+
+
+    def getVal(self, state):
+        l, h = self.getBlocks(state)
+        
+        if l == -1: # Out of bounds.
+            return 0
+        
+        return sum(self.blockWeights[l, h])/(h - l) # Take average to avoid weird edge effects. 
+    
+
+    def moveVal(self, state, target): #Moves prediction towards target; generalizes to neighbors, of course.
+        l, h = self.getBlocks(state)
+
+        if l == -1: # Out of bounds.
+            return None
+
+        v = self.getVal(state)
+        delta = target - v
+        gradStep = self.alpha*(delta / (h - l))
+        
+        for i in range(l, h):
+            self.blockWeights[i] += gradStep
+        
+        return None
+
+
---
+++ longRandomWalk.py
@@ -0,0 +1,43 @@
+import numpy as np
+from copy import deepcopy
+
+
+# Note that allowed states include 0 but not self.size
+
+class LRW:
+    """Numberline of size 'size' (default 1000), every turn, 
+steps up to stepsize right or left at random. Reward of 1 by terminating 
+on the right, or of -1 for terminating on the left."""
+
+    def __init__(self, size=1000, stepsize=100):
+        self.size = size
+        self.stepsize = stepsize
+        self.initial = int(size/2)
+        self.state = self.initial
+        self.complete = False
+    
+    def currentState(self):
+        return self.state, self.complete
+    
+    def move(self):
+        if self.complete:
+            print "Game over! Restart."
+            return self.state, 0, self.complete
+
+        step = np.random.randint(0 - self.stepsize, 
+                                 self.stepsize + 1)
+        self.state += step
+
+        if self.state < 0:
+            reward = -1
+            self.state = 0
+            self.complete = True
+        elif self.state >= self.size:
+            reward = 1
+            self.state = self.size
+            self.complete = True
+        else:
+            reward = 0
+        
+        return self.state, reward, self.complete
+     
